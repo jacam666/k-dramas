d87ae6880083c5dcbdbdaabcbef15ef3b8c8bb60
ja cam <ja6cam@gmail.com>
2025-06-16 17:25:14 +0100

    removed bg image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
       <div className="flex flex-col items-center pt-10 w-full  bg-center bg-cover opacity-90 rounded-lg shadow-lg"
       // style={{ backgroundImage: "url('/images/bg-image-3.jpeg')" }}
       >
-        <h1 className="font-Caprasimo text-3xl xl:text-7xl text-gray-50 font-bold">CAI-DRAMAS</h1>
-        <p className="text-gray-50 xl:text-4xl text-xl font-semibold text-center p-4">
+        <h1 className="font-[var(--font-caprasimo)] text-4xl xl:text-7xl text-gray-50 font-bold ">CAI-DRAMAS</h1>
+        <p className="font-[var(--font-caprasimo)] text-gray-50 xl:text-4xl text-xl font-semibold  text-center p-4">
           A LIST OF OUR FAVOURITE <br />K-DRAMAS
         </p>
       </div>
-      <div className='flex justify-center items-center mb-8'>
+      {/* <div className='flex justify-center items-center mb-8'>
         <Image
           src="/images/bg-image-3.jpeg"
           alt="Background Image"
@@ -20,11 +20,10 @@ export default function Home() {
           height={1080}
           className="w-full h-60 object-cover opacity-90 rounded-lg shadow-lg"
         />
-      </div>
+      </div> */}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-2 md:p-10">
         <div>
-          <h2 className='text-xl text-white font-bold text-center'>Alchemy Of Souls</h2>
           <DramaCarousel images={[
             '/images/alchemy-image-2.jpeg',
             '/images/alchemy-image-3.jpg',
@@ -33,55 +32,57 @@ export default function Home() {
             '/images/alchemy-image-6.webp',
             '/images/alchemy-image-7.webp',
           ]} />
+          <h2 className='text-xl text-white font-bold text-center'>Alchemy Of Souls</h2>
         </div>
 
         <div>
-          <h2 className='text-xl text-white font-bold text-center'>Dear Hongrang</h2>
           <DramaCarousel images={[
             '/images/hongrang-image-3.jpg',
             '/images/hongrang-image-2.webp',
             '/images/hongrang-image-4.webp',
             '/images/hongrang-image-5.jpg',
           ]} />
+          <h2 className='text-xl text-white font-bold text-center'>Dear Hongrang</h2>
         </div>
 
         <div>
-          <h2 className='text-xl text-white font-bold text-center'>Extraordinary Attorney Woo</h2>
           <DramaCarousel images={[
             // '/images/woo-image-1.jpg',
             '/images/woo-image-2.jpg',
             '/images/woo-image-3.png',
             '/images/woo-image-4.webp',
           ]} />
+          <h2 className='text-xl text-white font-bold text-center'>Extraordinary Attorney Woo</h2>
         </div>
 
         <div>
-          <h2 className='text-xl text-white font-bold text-center'>Vincenzo</h2>
           <DramaCarousel images={[
             // '/images/vincenzo-image-1.jpg',
             '/images/vincenzo-image-2.webp',
             '/images/vincenzo-image-3.jpg',
             '/images/vincenzo-image-4.webp',
           ]} />
+          <h2 className='text-xl text-white font-bold text-center'>Vincenzo</h2>
         </div>
 
         <div>
-          <h2 className='text-xl text-white font-bold text-center'>Weak Hero</h2>
           <DramaCarousel images={[
             '/images/weak-hero-image-1.jpg',
             '/images/weak-hero-image-3.webp',
             '/images/weak-hero-image-4.webp',
           ]} />
+          <h2 className='text-xl text-white font-bold text-center'>Weak Hero</h2>
         </div>
 
         <div>
-          <h2 className='text-xl text-white font-bold text-center'>Hometown Cha Cha Cha</h2>
           <DramaCarousel images={[
             // '/images/hometown-image-1.jpg',
             '/images/hometown-image-2.jpg',
             '/images/hometown-image-3.webp',
             '/images/hometown-image-4.webp',
           ]} />
+          <h2 className='text-xl text-white font-bold text-center'>Hometown Cha Cha Cha</h2>
+
         </div>
       </div>
     </div>
